9bead5f856a63893bf222e4880285e9c30463093
Cedric Taschereau <cedric.taschereau@boreal-is.com>
2018-01-01 22:12:24 -0500

    Add favicon

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 let router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Pi Leaf' });
 });
