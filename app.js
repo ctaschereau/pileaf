17893a945d6e5d3483e8572f1926ae85487d6006
Cédric Taschereau <cedric.taschereau@boreal-is.com>
2018-11-28 22:53:44 -0500

    remove sass

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const sassMiddleware = require('node-sass-middleware');
 
 process.on('uncaughtException', (err) => {
 	console.error(`uncaughtException : ${err.stack}`);
@@ -30,12 +29,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-	src: path.join(__dirname, 'public'),
-	dest: path.join(__dirname, 'public'),
-	indentedSyntax: true, // true = .sass and false = .scss
-	sourceMap: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
